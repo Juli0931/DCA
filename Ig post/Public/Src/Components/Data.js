6fcea8b23d25e465d49181e5de478e3ef8c64fc8
Juliana <56936845+Juli0931@users.noreply.github.com>
2022-09-02 22:33:26 -0500

    Añadí el json de los usuarios de ig

---
+++ Ig post/Public/Src/Components/Data.js
@@ -0,0 +1,92 @@
+const Data = [
+    {
+        "pictureprofile": "https://images.pexels.com/photos/2694130/pexels-photo-2694130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Juliibc_",
+        "ubi":"Cartago, Valle",
+        "content": "https://images.pexels.com/photos/3866555/pexels-photo-3866555.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "7982 Likes",
+        "description":"Borracha pero buena muchacha <3",
+        "coments":"View all 621 coments",
+        "date":"2 WEEKS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/4519122/pexels-photo-4519122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Sebas_01",
+        "ubi":"Cali, Valle",
+        "content": "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "1322 Likes",
+        "description":"Go big or go home",
+        "coments":"View all 214 coments",
+        "date":"3 MONTHS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Luna_Love",
+        "ubi":"Miami",
+        "content": "https://images.pexels.com/photos/375880/pexels-photo-375880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "7184 Likes",
+        "description":"Quería perder peso, pero no me gusta perder",
+        "coments":"View all 613 coments",
+        "date":"2 HOURS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "ShanMi",
+        "ubi":"Yumbo",
+        "content": "https://images.pexels.com/photos/1758144/pexels-photo-1758144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "792 Likes",
+        "description":"¡Bienvenida Primavera!",
+        "coments":"View all 94 coments",
+        "date":"9 DAYS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Sophie101",
+        "ubi":"Roldanillo",
+        "content": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "8826 Likes",
+        "description":"Daddy issues",
+        "coments":"View all 666 coments",
+        "date":"1 WEEK AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/556669/pexels-photo-556669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "CharlieDust",
+        "ubi":"Cali, Valle",
+        "content": "https://images.pexels.com/photos/2694130/pexels-photo-2694130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "79482 Likes",
+        "description":"Feeling hot hot hot",
+        "coments":"View all 6021 coments",
+        "date":"5 YEARS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Santygr2005",
+        "ubi":"Cartago, Valle",
+        "content": "https://images.pexels.com/photos/1755385/pexels-photo-1755385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "2182 Likes",
+        "description":"Toes in the sand",
+        "coments":"View all 321 coments",
+        "date":"28 MARCH 2005"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/702139/pexels-photo-702139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Herms",
+        "ubi":"London",
+        "content": "https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "7982 Likes",
+        "description":"Messy hair don’t care",
+        "coments":"View all 621 coments",
+        "date":"8 DAYS AGO"
+    },
+    {
+        "pictureprofile": "https://images.pexels.com/photos/38289/portrait-photography-profile-face-one-38289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "user": "Juanse7u7",
+        "ubi":"Cali",
+        "content": "https://images.pexels.com/photos/904276/pexels-photo-904276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "like": "1169 Likes",
+        "description":"Ya mucho desarrollo de personaje",
+        "coments":"View all 717 coments",
+        "date":"2 DAYS AGO"
+    }
+]
